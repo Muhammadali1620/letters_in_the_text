fbed6aa65a625bd3446e6f277277a106f094a05e
Muhammadali1620 <muhammadvalievmuhammadali@gmail.com>
2024-09-21 19:55:02 +0500

    debug

--- input_text.py
+++ input_text.py
@@ -2,13 +2,11 @@ def letters_in_the_text(text):
     letters = {}
     result = ""
     for i in text:
-        if not i.isalpha():
-            print("Xariflarda kiriting!")
-            return letters_in_the_text(input("So'z kiriting: "))
-        if i in letters:
-            letters[i] += 1
-        else:
-            letters[i] = 1
+        if i.isalpha():
+            if i in letters:
+                letters[i] += 1
+            else:
+                letters[i] = 1
     for key, value in letters.items():
         result += f"{key} = {value}\n"
     return result
